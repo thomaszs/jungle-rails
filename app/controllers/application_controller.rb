d80cf3f84a242a7dd47691ad84c3292237456d23
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-07-19 21:17:45 +0000

    Added basic http authentication for admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV["ADMIN_PASSWORD"], except: [:index] 
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
